2dbe13ae56440a1702439a7c2b465854035189b1
nxc4073 <nicholas.crouch@bjc.org>
2023-04-14 12:13:32 -0500

    wegotaguy

--- app/src/main/java/com/oneview/wegotaguy/MainActivity.kt
+++ app/src/main/java/com/oneview/wegotaguy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.oneview.wegotaguy
 
 import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.os.Bundle
 import android.app.Service
 import android.content.Context
@@ -8,6 +10,9 @@ import android.content.Intent
 import android.net.wifi.WifiManager
 import android.os.IBinder
 import android.os.PowerManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +24,39 @@ class MainActivity : AppCompatActivity() {
 
 class WifiService : Service() {
 
+    companion object {
+        const val NOTIFICATION_ID = 1
+        const val NOTIFICATION_CHANNEL_ID = "WifiServiceChannel"
+    }
     private lateinit var wifiLock: WifiManager.WifiLock
     private lateinit var wakeLock: PowerManager.WakeLock
 
     override fun onCreate() {
         super.onCreate()
-
+        // create notification channel if necessary
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            "Wi-Fi ADT Service",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager?.createNotificationChannel(channel)
         // On God
-        val intent = Intent(this, WifiService::class.java)
-        startService(intent)
+        //val intent = Intent(this, WifiService::class.java)
+        //startService(intent)
 
+        val intent = Intent(this, WifiService::class.java)
+        ContextCompat.startForegroundService(this, intent)
+        // build notification
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("Wi-Fi ADT Service")
+            .setContentText("We Got A Guy :^)")
+            .setSmallIcon(R.drawable.dcb)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        // start service in foreground mode
+        startForeground(NOTIFICATION_ID, notification)
         // get Wi-Fi manager
         val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
@@ -39,7 +67,7 @@ class WifiService : Service() {
         wifiLock = wifiManager.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "BJCWIFIISPERFECT")
 
         // create Wake lock
-        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Worksasdesigned")
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WORKSONMYMACHINE")
 
         // acquire locks
         wifiLock.acquire()
